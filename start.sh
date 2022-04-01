a04637c0e9add4a075b20e30b84991b7cf3d1713
dk [OFLINE] <98028377+vloggerdeven@users.noreply.github.com>
2022-04-01 19:21:30 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/saminsumesh/TeamSakura
+  git clone https://github.com/vloggerdeven/DKBOTxAutofilterBot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /TeamSakura
+  git clone $UPSTREAM_REPO /DKBOTxAutofilterBot
 fi
-cd /TeamSakura
+cd /DKBOTxAutofilterBot
 pip3 install -U -r requirements.txt
 echo "Starting Bot.... made by 𝘿𝙆 𝘽𝙊𝙏𝙭"
 python3 bot.py
