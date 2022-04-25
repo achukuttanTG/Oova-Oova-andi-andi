b5e492ce53823bc2519cd7db7f89ab24b3630cb5
achukuttanTG <104365884+achukuttanTG@users.noreply.github.com>
2022-04-25 18:36:08 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/vloggerdeven/DKBOTxAutofilterBot
+  git clone https://github.com/achukuttanTG/Oova-Oova-andi-andi
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /DKBOTxAutofilterBot
+  git clone $UPSTREAM_REPO /Oova-Oova-andi-andi
 fi
-cd /DKBOTxAutofilterBot
+cd /Oova-Oova-andi-andi
 pip3 install -U -r requirements.txt
 echo "Starting Bot.... made by 𝘿𝙆 𝘽𝙊𝙏𝙭"
 python3 bot.py
